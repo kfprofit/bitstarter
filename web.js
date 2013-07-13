707316cbb45bcf4c8b5add78914f839d4167d127
Maxim Nikolsky <ps@ps19.ru>
2013-07-13 04:03:01 +0000

    file reading added

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 
 app.get('/', function(request, response) {
- fs.readFile('index.html');
+buf = fs.readFile('index.html');
 
 response.send(buf.toString());
 });
