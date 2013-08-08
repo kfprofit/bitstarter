d7d35c9df544e86db40be1f0f49ee1bb2dae3788
Maxim Nikolsky <ps@ps19.ru>
2013-08-08 05:14:32 +0000

    test

--- web.js
+++ web.js
@@ -11,7 +11,7 @@ buf = fs.readFileSync('index.html');
 response.send(buf.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
  console.log("Listening on " + port);
 });
